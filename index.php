d62becbcfc131f79193238d6729575e91e455009
SilentThief <hexishgames@gmail.com>
2013-12-24 15:46:06 -0800

    + PHP Code
    Added code for getting pages for home, register, downloads, chat, vote,
    donate, and set the default page as home. Also, included top.php along
    with bottom.php (for style).

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+include_once('assets/config.php');
+
+$page = @$_GET['page'];
+switch ($page) {
+	case null:
+	case "home":
+		$getpage = "pages/home";
+		break;
+	case "register":
+		$getpage = "pages/register";
+		break;
+	case "downloads":
+		$getpage = "pages/downloads";
+		break;
+	case "chat":
+		$getpage = "pages/chat";	
+		break;
+	case "vote":
+		$getpage = "pages/vote";	
+		break;
+	case "donate":
+		$getpage = "pages/donate";	
+		break;			
+	default:
+		$getpage = "pages/home";
+		break;
+}
+
+include_once('assets/top.php');
+include_once($getpage.".php");
+include_once('assets/bottom.php');
+?>
