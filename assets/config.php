44eb29b9837a07ebcd16218067585c8ecdb0702d
SilentThief <hexishgames@gmail.com>
2013-12-24 15:49:50 -0800

    + Config & Nav Bar
    Added config code and the top navbar buttons.

---
+++ assets/config.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if(basename($_SERVER["PHP_SELF"]) == "config.php"){
+	die("Error!");
+} 
+$host['hostname'] 		 = "localhost";
+$host['user'] 	  		 = "root";
+$host['password'] 		 = "";
+$host['database'] 		 = "Simply Maple";
+$serverip  		 		 = "127.0.0.1";
+$loginport 		 		 = "8484";
+$servername 			 = "Simply Maple";
+$webtitle 				 = "Simply Maple";
+$timezone       	     = "US/Central";
+
+
+
+mysql_connect($host['hostname'],$host['user'],$host['password']) OR die("Can't connect to server.");
+mysql_select_db($host['database']) OR die("Mysql Database can't select the DB.");
+//Begin PHP Scripts
+function sql_injectionproof($sCode) {
+	if (function_exists("mysql_real_escape_string")) {
+		$sCode = mysql_real_escape_string($sCode);
+	} else {
+		$sCode = addslashes($sCode);
+	}
+	return $sCode;
+}
+?>
---
+++ assets/top.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title><?php echo $webtitle ?></title>
+	<link rel="stylesheet" href="assets/css/style.css"/>
+</head>
+		<div id="navbar">
+			<span class="navbar-button"><a href="?page=index">HOME</a></span>
+			<span class="navbar-button"><a href="?page=downloads">DOWNLOAD</a></span>
+			<span class="navbar-button"><a href="?page=register">REGISTER</a></span>
+			<span class="navbar-button"><a href="?page=donate">DONATE</a><span>
+			<span class="navbar-button"><a href="?page=vote">VOTE</a></span>
+			<span class="navbar-button"><a href="?page=forum">FORUM</a></span>
+			<span class="navbar-button"><a href="?page=chat">CHAT</a></span>
+		</div>
+</html>
