66422069198b7a48b81dc2a6ca38b2c891167bc3
SilentThief <hexishgames@gmail.com>
2013-12-24 16:13:25 -0800

    + Header Styles & Navbar Class Changes

--- assets/top.php
+++ assets/top.php
@@ -6,12 +6,12 @@
 	<link rel="stylesheet" href="assets/css/style.css"/>
 </head>
 		<div id="navbar">
-			<span class="navbar-button"><a href="?page=index">HOME</a></span>
-			<span class="navbar-button"><a href="?page=downloads">DOWNLOAD</a></span>
-			<span class="navbar-button"><a href="?page=register">REGISTER</a></span>
-			<span class="navbar-button"><a href="?page=donate">DONATE</a><span>
-			<span class="navbar-button"><a href="?page=vote">VOTE</a></span>
-			<span class="navbar-button"><a href="?page=forum">FORUM</a></span>
-			<span class="navbar-button"><a href="?page=chat">CHAT</a></span>
+			<span><a href="?page=index">HOME</a></span>
+			<span><a href="?page=downloads">DOWNLOAD</a></span>
+			<span><a href="?page=register">REGISTER</a></span>
+			<span><a href="?page=donate">DONATE</a><span>
+			<span><a href="?page=vote">VOTE</a></span>
+			<span><a href="?page=forum">FORUM</a></span>
+			<span><a href="?page=chat">CHAT</a></span>
 		</div>
 </html>
